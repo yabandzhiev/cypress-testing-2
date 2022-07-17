be059999d6831e0daacfa44280054a4143bffdb8
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-17 17:45:37 +0300

    backend test

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -3,7 +3,25 @@ describe("Test with backend", () => {
     cy.loginToApplication();
   });
 
-  it("should log in", () => {
-    cy.log("yes im logged in");
+  it("verify correct request and response", () => {
+    cy.intercept("POST", "**/articles").as("postArticles");
+
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("This is a title");
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the Article");
+    cy.contains("Publish Article").click();
+
+    cy.wait(1000);
+    cy.wait("@postArticles");
+    cy.get("@postArticles").then((xhr) => {
+      console.log(xhr);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the Article"
+      );
+      expect(xhr.response?.body?.article?.description).to.equal(
+        "This is a description"
+      );
+    });
   });
 });
